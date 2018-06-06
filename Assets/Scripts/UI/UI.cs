c4b29abbf6c487cd63d0b4275532d0e56560ba0e
Stephen Karpinskyj <stephen.karpinskyj@gmail.com>
2018-06-06 21:24:39 +1000

    Oscillating sprite test

---
+++ Assets/Scripts/UI/UI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    public void UGUI_HandleResetButtonClick()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+}
