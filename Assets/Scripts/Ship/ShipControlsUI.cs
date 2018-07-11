59b0440075e9f7eded6b74958f07f262fa37154a
Stephen Karpinskyj <stephen.karpinskyj@gmail.com>
2018-07-11 21:27:42 +1000

    Add UI ship controls

---
+++ Assets/Scripts/Ship/ShipControlsUI.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipControlsUI : MonoBehaviour
+{
+    [SerializeField]
+    GameObject controlsParent;
+    
+    [SerializeField]
+    Slider mainThrusterSlider;
+
+    [SerializeField]
+    Slider cwThrusterSlider;
+
+    [SerializeField]
+    Slider ccwThrusterSlider;
+    
+    [SerializeField]
+    Thruster mainThruster;
+
+    [SerializeField]
+    Thruster cwThruster;
+
+    [SerializeField]
+    Thruster ccwThruster;
+    
+    [SerializeField]
+    AnimationCurve thrusterCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+    
+    void Awake()
+    {
+        controlsParent.SetActive(false);
+    }
+
+    public void UGUI_HandleToggleControlsButtonClick()
+    {
+        controlsParent.SetActive(!controlsParent.activeSelf);
+    }
+
+    public void UGUI_HandleMainThrusterSliderValueChange()
+    {
+        mainThruster.SetThrottle(thrusterCurve.Evaluate(mainThrusterSlider.value));
+    }
+
+    public void UGUI_HandleCWThrusterSliderValueChange()
+    {
+        cwThruster.SetThrottle(thrusterCurve.Evaluate(cwThrusterSlider.value));
+    }
+    
+    public void UGUI_HandleCCWThrusterSliderValueChange()
+    {
+        ccwThruster.SetThrottle(thrusterCurve.Evaluate(ccwThrusterSlider.value));
+    }
+}
